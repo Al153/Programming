6b0aa0784e8461607e540773ad3c1514048813e1
Alexander Taylor <al.taylor1105@gmail.com>
2013-09-11 18:26:27 +0100

    debugging

---
+++ C/Arduino/gridMovement.c
@@ -0,0 +1,13 @@
+void move_to_point(int[2] start, int[2] end){
+	/* Assumes facing north */  
+	int dx;
+	int dy;
+
+	dx = end[0] - start[0];
+	dy = end[1] - start[1];
+
+	for (int i;i<dy;i++){
+		if 
+	}
+
+}
