ff7b6ad23742c9386421dfc156d2617d7e78e50b
Al153 <al.taylor1105@gmail.com>
2013-09-26 20:56:16 +0100

    Consolidated Pyfth, added new words and program traversal algorithm

--- Python/Interpreters and compilers/Pyfth/ForthTools.py
+++ Python/Interpreters and compilers/Pyfth/ForthTools.py
@@ -8,70 +8,86 @@ class Forth:
 	def __init__(self):
 		self.Data_Stack = []
 
-		self.intarrays = {} #stack data_types are always signed 16bit ints
-		self.chararrys = {}
-		self.chars = {}
-		self.ints = {}
+		self.defined_words = {}
+		self.tokens = []
+
+		self.return_stack = []
+		self.loop_stack = []
+		self.if_stack = []
+		self.defined_words = {}
+		self.arrays = {}
+
 
 		self.inbuilt_words = {
-							  "+":self.add,
-							  "-":self.subtract,
-							  "*":self.multiply,
-							  "/":self.divide,
-							  "//":self.floordivide
-							  "%":self.modulo,
+							  "+":self.add, 					#/
+							  "-":self.subtract,				#/
+							  "*":self.multiply,				#/
+							  "/":self.divide,					#/
+							  "//":self.floordivide				#/
+							  "%":self.modulo,					#/
+							  "<<":self.shiftleft,				#/
+							  ">>":self.shiftright,				#/
 
-							  "swap": self.swap,
-							  "dup": self.dup,
-							  "drop": self.drop,
-							  "clear": self.clear,
+							  "swap": self.swap,				#/
+							  "dup": self.dup,					#/
+							  "drop": self.drop,				#/
+							  "clear": self.clear,				#/
 
-							  "AND":self.AND,
-							  "OR": self.OR,
-							  "NOT": self.NOT,
-							  "XOR": self.XOR,
+							  "AND":self.AND,					#/
+							  "OR": self.OR,					#/
+							  "NOT": self.NOT,					#/
+							  "XOR": self.XOR,					#/
 
-							  "=":self.compareEqual,
-							  "<":self.compareSmallerthan,
-							  ">":self.compareGreaterthan,
-							  "if":self.If,
-							  "endif":self.endif,
-							  "for":self.FOR,
-							  "while":self.While,
-							  "loop":self.loop,
+							  "=":self.compareEqual,			#/
+							  "<":self.compareSmallerthan,		#/
+							  ">":self.compareGreaterthan,		#/
+							  "if":self.If,						#			need technical function
+							  "else":self.Else,					#			need technical function
+							  "endif":self.endif,				#/
+							  "for":self.FOR,					#			need technical function
+							  "while":self.While,				#			need technical function
+							  "loop":self.loop,					#			need technical function
 
-							  "word":self.word,
-							  "endword": self.endword,
-							  "local":self.local,
-							  "global":self.Global,
-							  "intarray":self.instarray,
-							  "chararry":self.chararray,
-							  "index":self.index,
+							  "return": self.Return,			#/
+							  "local_var":self.local,				#/
+							  "global_var":self.Global,				#/
+							  "local_array":self.array,			#			x
+							  "global_index":self.index,				#			x
+							  "insert":self.insert				#			x
 
-  							  "unsigned": self.unsigned,
-							  "int": self.int,
-							  "char": self.char,
+  							  "unsigned": self.unsigned,		#/
+							  "int": self.int,					#/
+							  "char": self.char,				#/
 
-							  "inInt":self.inInt,
-							  "self.inChars":self.inChars,
-							  "wait":self.wait,
-							  "print": self.Print,
-							  "printchar": self.printChar
+							  "inInt":self.inInt,				#			x
+							  "self.inChars":self.inChars,		#			x
+							  "wait":self.wait,					#			x
+							  "print": self.Print,				#			x
+							  "printchar": self.printChar 		#			x
 							  }
 
 		self.defined_words = {}
 		self.instruction_pointer = -1
 		self.tokens = []
 
-	def input_tokens(self, tokens):
-		self.tokens = tokens
-		self.instruction_pointer = -1
+		self.return_stack = []
+		self.defined_words = {}
+		self.arrays = {}
 
-	def sequential_execute(self):
-		self.instruction_pointer += 1
-		token = self.tokens[self.instruction_pointer]
-		if token in self.inbuilt_words:
-				self.inbuilt_words[token]()
+					#word no, word name
+		self.position = [0,		"main"]
+
+	#================ Error handling ======================
+	def unrecognised_word(self,word):
+		print """
+Error: Unrecognised word: """, word, " Found in word: ",self.position[1] 
+		quit()
+
+	def no_end_of_function(self):
+		print """
+Error:
+There is likely to be a missing name at end of '""",self.position[1]
+		quit()
 
 	def stack_empty(self):
 		print: """
@@ -81,12 +97,77 @@ ERROR: Stack is empty.
 """
 		quit()
 
+	
+	# ============== Setup =====================
+	def input_tokens(self, tokens):
+		self.tokens = tokens
+		self.instruction_pointer = -1
+
+	def gather(self):
+		"""
+		Gathers words within program for execution
+		"""
+		current_word = []
+		current_word_name = ""
+		found_word = 0
+		for token in self.tokens:
+			if found_word:
+				if token == ";":
+					found_word = 0
+					current_word.append("return")
+					self.defined_words[current_word_name] = current_word
+					current_word = []
+				else:
+					current_word.append(token)
+			else:
+				found_word = 1
+				current_word_name = token
+
+
+	# ============= technical ===============
 	def ifLoop(self):
 
+	def elseloop(self):
+
+	def forloop(self):
+
+	def whileloop(self):
+
+	# ============ Execution ===============
+
+	def run(self):
+		while 1:
+			self.execute()
+
+	def execute(self):
+		try:
+			word = self.defined_words[self.position[1]][self.position[0]]
+		except KeyError:
+			self.unrecognised_word(word)
+		except IndexError:
+			self.no_end_of_function()
+		try:
+			data = (float(word))
+			if data == int(word):
+				self.Data_stack.append(int(word))
+			else:
+				self.Data_stack.append(data)
+			self.position[0]+=1
+		except ValueError:
+			try:
+				self.inbuilt_words[word]()
+				self.position[0]+=1
+			except KeyError:
+				if self.position[0]+"."+word in self.defined_words:
+					word = self.position[0]+"."+word
+				destination = [word,0]
+				self.position[0] += 1
+				self.return_stack.append(self.position)
+				self.position = destination
 
 
 
-
+	# =================== individual inbuilt words =========
 	def add(self):
 		try:
 			var1 = self.Data_Stack.pop()
@@ -108,7 +189,7 @@ ERROR: Stack is empty.
 			self.stack_empty()
 			
 		finally:
-			var3 = var1-var2
+			var3 = var2 - var1
 			Data_Stack.append(var3)
 
 	def multiply(self):
@@ -133,7 +214,7 @@ ERROR: Stack is empty.
 			
 		finally:
 			try:
-				var3 = var1/float(var2)
+				var3 = var2/float(var1)
 			except:
 				self.divide_by_zero()
 			finally:
@@ -149,7 +230,7 @@ ERROR: Stack is empty.
 			
 		finally:
 			try:
-				var3 = int(var1/var2)
+				var3 = int(var2/var1)
 			except:
 				self.divide_by_zero()
 			finally:
@@ -165,11 +246,35 @@ ERROR: Stack is empty.
 			
 		finally:
 			try:
-				var3 = var1%var2
+				var3 = var2%var1
 			except:
 				self.divide_by_zero()
 			finally:
 				Data_Stack.append(var3)
+
+	def shiftleft(self):
+		try:
+			var1 = self.Data_Stack.pop()
+			var2 = self.Data_Stack.pop()
+
+		except:
+			self.stack_empty()
+			
+		finally:
+			var3 = var2 << var1
+			Data_Stack.append(var3)
+
+	def shiftright(self):
+		try:
+			var1 = self.Data_Stack.pop()
+			var2 = self.Data_Stack.pop()
+
+		except:
+			self.stack_empty()
+			
+		finally:
+			var3 = var2 >> var1
+			Data_Stack.append(var3)
 
 
 	def swap(self):
@@ -316,5 +421,45 @@ ERROR: Stack is empty.
 		finally:
 			if not var1:
 				self.ifLoop()
+
+	def Else(self):
+
+		self.else_loop()
+
+	def endif(self):
+
+		pass
+
+
+
+	def unsigned(self):
+		try:
+			var1 = self.Data_Stack.pop()
+
+		except:
+			self.stack_empty()
+
+		finally:
+			self.Data_Stack.append(abs(var1))
+
+	def int(self):
+		try:
+			var1 = self.Data_Stack.pop()
+
+		except:
+			self.stack_empty()
+
+		finally:
+			self.Data_Stack.append(int(var1))
+
+	def char(self):
+		try:
+			var1 = self.Data_Stack.pop()
+
+		except:
+			self.stack_empty()
+
+		finally:
+			self.Data_Stack.append(int(var1)%256)
 
 
--- Python/Interpreters and compilers/Pyfth/tree traversal.py
+++ Python/Interpreters and compilers/Pyfth/tree traversal.py
@@ -43,38 +43,22 @@ There is likely to be a missing name at end of '""",self.position[1]
 		except IndexError:
 			self.no_end_of_function()
 		try:
-			self.Data_stack.append(int(word))
+			data = (float(word))
+			if data == int(word):
+				self.Data_stack.append(int(word))
+			else:
+				self.Data_stack.append(data)
 			self.position[0]+=1
 		except ValueError:
 			try:
 				self.inbuilt_words[word]()
 				self.position[0]+=1
 			except KeyError:
-					
+				if self.position[0]+"."+word in self.defined_words:
+					word = self.position[0]+"."+word
 				destination = [word,0]
 				self.position[0] += 1
 				self.return_stack.append(self.position)
 				self.position = destination
 
-	def Return(self):
-		self.postition = self.return_stack.pop()
 
-	def Global(self):
-		try:
-			var1 = self.Data_stack.pop()
-		except:
-			self.empty_stack()
-		finally: #A non existent var_name can never cause a problem, as return becomes the var_name
-				self.position[0] += 1
-				var_name = self.defined_words[self.position[1]][self.position[0]]
-				self.defined_words[var_name] = [str(var1)]
-
-	def Local(self):
-		try:
-			var1 = self.Data_stack.pop()
-		except:
-			self.empty_stack()
-		finally: #A non existent var_name can never cause a problem, as return becomes the var_name
-				self.position[0] += 1
-				var_name = self.position[1]+"."+self.defined_words[self.position[1]][self.position[0]]
-				self.defined_words[var_name] = [str(var1)]
