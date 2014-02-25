57d5019c3a713722e8fc155925a72acb8b3b28c1
Al153 <al.taylor1105@gmail.com>
2014-02-25 18:37:35 +0000

    Work on CPU 10

--- Python/CPU project/CPU 10/ALU.py
+++ Python/CPU project/CPU 10/ALU.py
@@ -1,3 +1,44 @@
 class ALU:
-	def __init__(self):
-		pass
+	def __init__(self,input_bus,output_bus,op_bus):
+		self.input_bus = input_bus
+		self.output_bus = output_bus
+		self.op_bus = op_bus
+
+		self.reg1 = 0
+		self.reg2 = 0
+		self.flags = 0
+
+
+
+
+
+	def Add(self):
+
+	def Sub(self):
+
+	def Mul(self):
+
+	def Div(self):
+
+	def Mod(self):
+
+	def AND(self):
+
+	def OR(self):
+
+	def XOR(self):
+
+	def NOT(self):
+
+	def NAND(self):
+
+	def NOR(self):
+
+	def XNOR(self):
+
+	def SHL(self):
+
+	def SHR(self):
+
+	def CMP(self):
+
--- Python/CPU project/CPU 10/CPU.py
+++ Python/CPU project/CPU 10/CPU.py
@@ -23,7 +23,12 @@ __________________ Registers __________________
 	0x2 		Accumulator:   stores the result of most recent multiplication
 	0x3			jump register: the original value of the program counter gets moved into this
 	0x4			Program counter: stores address of the next instruction to execute
-    0x5 		Flags
+    0x5 		Flags set 	- data written here will be ORed with the data in the flags register
+    0x6			Flags reset - data written here will be ANDed with the data in the flags register
+    0x7			Stack pointer - reserved for stack use (but stack is not built into hardware)
+    0x8 		Stack base - reserved for the bottom part of the stack.
+
+    0x9 ==> 0xf GP registers (0...6)
 
 
 		
@@ -36,16 +41,21 @@ ____ instruction format ____
 byte:   0    	1    	2    	3    			4    	5   	 6  	  7
 
 REG     Opcode  Reg1   reg2 	conditional	    A/D	    A/D 	 A/D 	  A/D
-L/S 	Opcode  Reg1   index     conditional     A/D	    A/D 	 A/D 	  A/D 
+L/S 	Opcode  Reg1   index    conditional     A/D	    A/D 	 A/D 	  A/D 
+
+bit [0] of conditional triggers a conditional command if high
+bit [1] of conditional triggers an "if x:" conditional with bits [4 ==> 7] as register address
+otherwise bits [4 ==> 7] as the conditional flag to check
 
 
 Opcode   Mnemonic   Type    Notes
 00		 Halt  		N/A
-01		 Pass		N/A
-02       CMP		REG 
-03		 MOV        REG
-04		 LOD		L/S
-05		 STR		L/S
+01		 Pass		N/A 
+02		 MOV        REG
+03		 LOD		L/S
+04		 STR		L/S
+05       CMP		REG
+06 		 CMP 		L/S
 
 .
 .
@@ -65,6 +75,8 @@ Opcode   Mnemonic   Type    Notes
 1b		 XNOR		REG
 1c		 SHL		REG
 1d		 SHR		REG
+1e		 ADDc 		REG
+1f 		 SUBb 		REG
 
 .
 .
@@ -84,6 +96,9 @@ Opcode   Mnemonic   Type    Notes
 2b		 XNOR		L/S
 2c		 SHL		L/S
 2d		 SHR		L/S
+2e 		 ADDc 		L/S
+2f		 SUBb 		L/S
 
 
+__________________ Flags __________________
 
