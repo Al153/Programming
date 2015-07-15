23eded855097c354bfafab6f3a75aa41477deccb
Al153 <al.taylor1105@gmail.com>
2015-07-15 23:49:12 +0100

    still debugging fourier transforms

--- Python/CPU project/CPU 10/Python test/fft.py
+++ Python/CPU project/CPU 10/Python test/fft.py
@@ -1,26 +1,9 @@
+import cmath
 import math
-#def fft(x,N,width,offset): #x is input, X is output
-#	print N,width,offset
-#	if N == 1:
-#		return
-#	else:
-#		if N&1:
-#			raise ValueError("N wmust be a power of 2")
-#		
-#		#divide and conquer
-#		fft(x,N/2,width*2,offset+0)			#applies fft to even and odd arrays
-#		fft(x,N/2,width*2,offset+width)
-#		for k in xrange(N/2):
-#			t = x[k*width+(offset+1)*width*2]*math.e**complex(0,-2*math.pi*k/N)
-#			even_value = x[k*width*2+offset*width]
-#			x[k*width +offset*width] = even_value + t
-#			x[(k+N/2)*width +offset*width] = even_value - t
-#		return 
-#a =[1,0]
-#fft(a,len(a),1,0)
-#print a
 
-from numpy.fft import fft
-from numpy import array
-a = array([1.0, 0.0])
-print fft(a)
+
+def dft(X):
+	N = len(X)
+	return [sum([X[n]*cmath.exp(complex(0,-2*math.pi*k*n/N)) for n in xrange(N)])for k in xrange(N)]
+
+print dft([1,0])
