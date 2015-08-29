17249e997daaebdc0ff826ae23230258558700e0
Al153 <al.taylor1105@gmail.com>
2015-08-29 09:17:58 +0100

    more efficient vigenere7

--- Python/Python golf/vigenere - 2.py
+++ Python/Python golf/vigenere - 2.py
@@ -1,7 +1,7 @@
-encrypt = lambda (text,mode,key,pointer,key_pointer,alphabet,result:  result if pointer == len(text) else encrypt(text,mode,key,pointer + 1,(key_pointer+1)%len(key) if text[pointer] in alphabet else key_pointer, alphabet,(result + alphabet[(alphabet.index(text[pointer]) + alphabet.index(key[key_pointer]))%26] if mode == "encrypt" else result + alphabet[(alphabet.index(text[pointer])%26 - alphabet.index(key[key_pointer])%26)%26]) if key[key_pointer] in alphabet and text[pointer] in alphabet else result))
-print encrypt(open("file.txt").read(),"encrypt","lemon",0,0,('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'),'')
+e = lambda (t,m,k,p,l,r):  r if p == len(t) else e(t,m,k,p + 1,(l+1)%len(k) if (96<ord(t[p])<123) else l, (r + chr((ord(t[p])+ord(k[l])-194)%26+97) if m == "e" else r + chr((ord(t[p])-ord(k[l]))%26+97) if (96<ord(k[l])<123)and(96<ord(t[p])<123) else r))
+print e(open("file.txt").read().lower(),"e","lemon",0,0,'')
 
-##plaintext,key,mode
+##plaint,k,m
 ##print hello
 #import sys
 #print str(map(lambda char: ('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')[char%26],
@@ -11,7 +11,7 @@ print encrypt(open("file.txt").read(),"encrypt","lemon",0,0,('a','b','c','d','e'
 #	for i in xrange(len(sys.argv[0]))]#
 #
 
-#	)) if sys.argv[2] == "encrypt" else  str(map(lambda char: ('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')[char%26],
+#	)) if sys.argv[2] == "e" else  str(map(lambda char: ('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')[char%26],
 #	 [
 #	 ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z').index(sys.argv[0][i]) - 
 #	 ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z').index(sys.argv[1][i%len(sys.argv[1])])
